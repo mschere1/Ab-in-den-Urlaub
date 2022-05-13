67679a66540af7becdfe286026f82386e943973d
Bastian Huber <bhuber01@sstud.hs-offenburg.de>
2022-05-13 15:38:26 +0200

    d

---
+++ Backend/AbInDenUrlaub/AbInDenUrlaub/Models/nutzer.cs
@@ -0,0 +1,16 @@
+﻿namespace AbInDenUrlaub.Models
+{
+    public class nutzer
+    {
+        public int user_id { get; set; }
+        public string username { get; set; }
+        public string name  { get; set; }
+        public string nachname  { get; set; }
+        public string passwort { get; set; }
+        public string email { get; set; }
+        public int tokenstand { get; set; }
+        public bool vermieter  { get; set; }
+        public bool admin { get; set; }
+    }
+
+}
